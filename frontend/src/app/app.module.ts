692be317b3d642b4d8568aebb9392598db2dc918
Peter Matov <pmatov@gmail.com>
2023-05-31 15:15:17 +0300

    [C] Update loan component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoanFormComponent } from './loan-form/loan-form.component';
 import { LoanListComponent } from './loan-list/loan-list.component';
 import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoanFormComponent,
     LoanListComponent,
-    PaymentFormComponent
+    PaymentFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/loan-form/loan-form.component.ts
+++ frontend/src/app/loan-form/loan-form.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-loan-form',
   templateUrl: './loan-form.component.html',
-  styleUrls: ['./loan-form.component.sass']
+  styleUrls: ['./loan-form.component.sass'],
 })
 export class LoanFormComponent {
+  borrowerName = '';
+  amount = '';
+  term = '';
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const loan = {
+      borrowerName: this.borrowerName,
+      amount: this.amount,
+      term: this.term,
+    };
+
+    this.http.post('http://localhost:3000/loans', loan).subscribe(
+      (response) => console.log(response),
+      (error) => console.error(error)
+    );
+  }
 }
